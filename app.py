4c9f1b2c430fea10398fff4a0f3e4afaf0e14f9d
Adrian <adrian_zinko@op.pl>
2020-04-08 16:25:31 +0200

    feat: request data with get and post method

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+entries = []
 
-
-@app.route("/home")
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return "Hello, People!"
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        entries.append(entry_content)
+    
+    return render_template('home.html')
